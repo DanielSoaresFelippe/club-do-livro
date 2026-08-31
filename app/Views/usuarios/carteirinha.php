234442d063f05cda13e6373c38d9a8ff1b0d059c
gleiceklly <gleicekellymatosribeiro@gmail.com>
2026-08-31 13:41:16 -0300

    adicionando detalhes na view de historico

--- app/Views/usuarios/carteirinha.php
+++ app/Views/usuarios/carteirinha.php
@@ -101,6 +101,26 @@
     </div>
 </section>
 
+<div class="nuvem-divisor nuvem-divisor--historico">
+    <svg viewBox="0 0 1440 90" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill="var(--blue-historico)" d="
+            M0,90
+            Q60,10 120,90
+            Q180,10 240,90
+            Q300,10 360,90
+            Q420,10 480,90
+            Q540,10 600,90
+            Q660,10 720,90
+            Q780,10 840,90
+            Q900,10 960,90
+            Q1020,10 1080,90
+            Q1140,10 1200,90
+            Q1260,10 1320,90
+            Q1380,10 1440,90
+            L1440,90 Z"/>
+    </svg>
+</div>
+
 <section class="conteudo-pastas" id="conteudoPastas">
     <div class="painel-pasta painel-ativo" data-painel="historico">
         <div class="painel-conteudo">
